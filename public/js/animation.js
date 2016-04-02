baa4db0439b39a98dfd3de27b76b7e7774b8150a
mehdiH <medh31@gmail.com>
2016-04-02 13:36:40 +0200

    Possibilité de d'ajouté un article dans la page index

--- public/js/animation.js
+++ public/js/animation.js
@@ -11,5 +11,17 @@ $(document).ready(function(){
 		event.preventDefault();
 		$('.mh-ajout-article').toggle();
 		$('.mh-article').toggle();
+		
 	})
+
+	$('.mh-publie').click(function(event){
+		var title = $('#title').val();
+		var description = $('#description').val();
+		var price = $('#price').val();
+		var image = $('#image').val();
+		$('#mh-premier-article').append('<div class="container mh-article"><div class="row"><img src="' + image + '" alt="image perceuse" class="col-md-3"><div class="col-md-5"><h3>' + title + '</h3><h4>' + price + '</h4></div><div><img src="http://www.location-orcieres.fr/img/icone_favoris.png" alt="Favori" class="col-md-offset-2 col-md-2"></div></div><div class="row"><button class="col-md-offset-10 col-md-2 btn btn-success">je peux faire mieux</button></div></div>');
+	})	
+	
+	
+
 });
